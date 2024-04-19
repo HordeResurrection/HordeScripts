2fc0c9a6a0e5538c536ab1e2af3ab925431a44c9
Horde Resurrection <65279361+HordeResurrection@users.noreply.github.com>
2024-04-19 18:26:29 +0300

    MasterMind: add DisableBuildPlaceChecking parameter to produceRequest

--- Scripts/examples/mastermind-request.ts
+++ Scripts/examples/mastermind-request.ts
@@ -6,36 +6,44 @@ import HordeExampleBase from "./base-example";
  * Пример работы с MasterMind
  */
 export class Example_MasterMindRequest extends HordeExampleBase {
+    workPlayerNum: string;
+    printRequestsPeriod: number;
+    masterMind: any;
 
     public constructor() {
         super("Request for MasterMind");
+        
+        this.workPlayerNum = "1";
+        this.printRequestsPeriod = 1000;
     }
 
     public onFirstRun() {
         this.logMessageOnRun();
         
-        let realPlayer = Players["1"].GetRealPlayer();
-        let masterMind = realPlayer.MasterMind;
-        if (!masterMind) {
+        let workPlayer = Players[this.workPlayerNum].GetRealPlayer();
+        this.masterMind = workPlayer.MasterMind;
+        if (!this.masterMind) {
             this.log.info('Выбранный игрок не управляется MasterMind.');
             return;
         }
-
+        
         // Активация MasterMind, если отключен
-        if (!masterMind.IsWorkMode) {
-            this.log.info('Включение режима работы MasterMind для', realPlayer.Nickname);
-            masterMind.IsWorkMode = true;
+        if (!this.masterMind.IsWorkMode) {
+            this.log.info('Включение режима работы MasterMind для', workPlayer.Nickname);
+            this.masterMind.IsWorkMode = true;
         }
 
+        // Объект для задания запросов
+        let productionDepartament = this.masterMind.ProductionDepartment;
+
         // Создадим запрос на производство одной катапульты
-        let productionDepartament = masterMind.ProductionDepartment;
         let catapultCfg = HordeContentApi.GetUnitConfig("#UnitConfig_Slavyane_Catapult");
 
         // Параметры запроса
         let produceRequestParameters = new ProduceRequestParameters(catapultCfg, 1);
         produceRequestParameters.CheckExistsRequest = false;            // Следует ли проверять наличие имеющихся запросов?
         produceRequestParameters.AllowAuxiliaryProduceRequests = true;  // Разрешить ли создавать запросы на производство требуемых юнитов?
-        produceRequestParameters.TargetPosition = null;                 // Местоположение строительства (актуально только для зданий)
+        produceRequestParameters.TargetCell = null;                     // Местоположение строительства (актуально только для зданий)
 
         // Добавление запроса
         if (!productionDepartament.AddRequestToProduce(produceRequestParameters)) {
@@ -45,15 +53,15 @@ export class Example_MasterMindRequest extends HordeExampleBase {
         }
 
         // Создадим запрос на производство одной избы
-        let productionDepartament = masterMind.ProductionDepartment;
         let farmCfg = HordeContentApi.GetUnitConfig("#UnitConfig_Slavyane_Farm");
 
         // Параметры запроса
         produceRequestParameters = new ProduceRequestParameters(farmCfg, 1);
         produceRequestParameters.CheckExistsRequest = false;            // Следует ли проверять наличие имеющихся запросов?
         produceRequestParameters.AllowAuxiliaryProduceRequests = false; // Разрешить ли создавать запросы на производство требуемых юнитов?
-        produceRequestParameters.TargetPosition = createPoint(95, 3);   // Местоположение строительства (верхний левый угол)
+        produceRequestParameters.TargetCell = createPoint(95, 3);       // Местоположение строительства (верхний левый угол)
         produceRequestParameters.MaxRetargetAttempts = 0;               // Количество попыток (за такт) для выбора другого места строительства поблизости
+        produceRequestParameters.DisableBuildPlaceChecking = true;      // Принудительное строительство в этой клетке без проверки места
 
         // Добавление запроса
         if (!productionDepartament.AddRequestToProduce(produceRequestParameters)) {
@@ -63,10 +71,30 @@ export class Example_MasterMindRequest extends HordeExampleBase {
         }
 
         // Проверяем запросы
-        let requests = masterMind.Requests;
+        let requests = this.masterMind.Requests;
         this.log.info('Запросов в обработке:', requests.Count);
         ForEach(requests, item => {
             this.log.info('-', item);
         });
+    }
+
+    public onEveryTick(gameTickNum: number) {
+        if (this.masterMind == 0) {
+            return;
+        }
+        if (this.printRequestsPeriod == 0) {
+            return;
+        }
+        if (gameTickNum % this.printRequestsPeriod != 0) {
+            return;
+        }
+
+        // Отобразить текущие запросы
+        let requests = this.masterMind.Requests;
+        this.log.info('Запросов в обработке:', requests.Count);
+        ForEach(requests, item => {
+            this.log.info('-', item);
+        });
+        return;
     }
 }
