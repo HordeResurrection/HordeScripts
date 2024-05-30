3c7d9d979743f57ed4fb1faaee04bef377eb15ef
night-ghost-1 <nightgho5t@yandex.ru>
2024-05-30 23:41:59 +0300

    Add logging

--- Mods/Mara/SettlementControllerStates/RoutingState.ts
+++ Mods/Mara/SettlementControllerStates/RoutingState.ts
@@ -1,11 +1,7 @@
-// import { MaraResourceCluster, MaraResourceMap, MaraResourceType } from "../MaraResourceMap";
-// import { TargetExpandData } from "../MaraSettlementController";
 import { MaraResourceMap, MaraResourceType } from "../MaraResourceMap";
 import { SettlementControllerStateFactory } from "../SettlementControllerStateFactory";
 import { MaraResources } from "../Utils/Common";
 import { MaraUtils } from "../Utils/MaraUtils";
-// import { MaraPoint, MaraResources } from "../Utils/Common";
-// import { MaraUtils, UnitComposition } from "../Utils/MaraUtils";
 import { MaraSettlementControllerState } from "./MaraSettlementControllerState";
 
 export class RoutingState extends MaraSettlementControllerState {
@@ -33,27 +29,31 @@ export class RoutingState extends MaraSettlementControllerState {
         let resourcesToMine = new MaraResources(0, 0, 0, 0);
 
         if (resources.People < 10) {
-            this.settlementController.Debug
+            this.settlementController.Debug(`Low people`);
             needExpand = true;
             resourcesToMine.People = 1;
         }
         
         if (resources.Gold < 1000 && leftResources.has(MaraResourceType.Gold)) {
+            this.settlementController.Debug(`Low gold`);
             needExpand = true;
             resourcesToMine.Gold = 1;
         }
         
         if (resources.Metal < 1000 && leftResources.has(MaraResourceType.Metal)) {
+            this.settlementController.Debug(`Low metal`);
             needExpand = true;
             resourcesToMine.Metal = 1;
         }
 
         if (resources.Wood < 1000 && leftResources.has(MaraResourceType.Wood)) {
+            this.settlementController.Debug(`Low lumber`);
             needExpand = true;
             resourcesToMine.Wood = 1;
         }
 
         if (needExpand) {
+            this.settlementController.Debug(`Low on one or more resource, proceeding to expand...`);
             this.fillExpandData(resourcesToMine);
             this.settlementController.State = SettlementControllerStateFactory.MakeExpandPrepareState(this.settlementController);
             return;
@@ -73,6 +73,7 @@ export class RoutingState extends MaraSettlementControllerState {
                 }
             }
             else {
+                this.settlementController.Debug(`No produceable combat units, developing...`);
                 this.settlementController.State = SettlementControllerStateFactory.MakeDevelopingState(this.settlementController);
             }
             
