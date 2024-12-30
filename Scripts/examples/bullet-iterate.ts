7e395957caa10a0c539a27da4f2017527af275c0
Horde Resurrection <65279361+HordeResurrection@users.noreply.github.com>
2024-12-30 19:23:00 +0300

    improve bullet-iterate example: add condition to automatic turning off

--- Scripts/examples/bullet-iterate.ts
+++ Scripts/examples/bullet-iterate.ts
@@ -1,8 +1,8 @@
-import { mergeFlags } from "library/dotnet/dotnet-utils";
-import { BindingFlags } from "library/dotnet/godmode/reflection";
-import { BaseBullet } from "library/game-logic/horde-types";
 import HordeExampleBase from "./base-example";
 
+
+// Количество обработанных событий, когда будут отключены обработчики
+const HITS_TO_STOP_EVENT_HANDLERS = 20;
 
 /**
  * Пример итерирования текущих снарядов на сцене.
@@ -13,28 +13,28 @@ import HordeExampleBase from "./base-example";
  */
 export class Example_IterateBullets extends HordeExampleBase {
 
-    private bulletsRegistry : any;
-    
+    private eventHits = 0;
+        
     public constructor() {
         super("Iterate bullets");
-        
-        let realScena = ActiveScena.GetRealScena();
-
-        this.bulletsRegistry = realScena.Bullets;
     }
 
     public onFirstRun() {
         this.logMessageOnRun();
         
-        this.log.info('Реестр снарядов:', this.bulletsRegistry);
+        this.eventHits = 0;
+
+        let bulletsRegistry = ActiveScena.Bullets;
+        this.log.info('Реестр снарядов:', bulletsRegistry);
         
         let that = this;
 
         if (this.globalStorage.currentAddedHandler) {
             this.globalStorage.currentAddedHandler.disconnect();
         }
-        this.globalStorage.currentAddedHandler = this.bulletsRegistry.ItemAdded.connect(function(sender, args) {
+        this.globalStorage.currentAddedHandler = bulletsRegistry.ItemAdded.connect(function(sender, args) {
             try {
+                that.eventHits++;
                 let bull = args.Item;
                 that.log.info('- Снаряд добавлен:', '[' + bull.State + ']', bull);
             } catch (ex) {
@@ -45,13 +45,29 @@ export class Example_IterateBullets extends HordeExampleBase {
         if (this.globalStorage.currentRemovedHandler) {
             this.globalStorage.currentRemovedHandler.disconnect();
         }
-        this.globalStorage.currentRemovedHandler =this.bulletsRegistry.ItemRemoved.connect(function(sender, args) {
+        this.globalStorage.currentRemovedHandler = bulletsRegistry.ItemRemoved.connect(function(sender, args) {
             try {
+                that.eventHits++;
                 let bull = args.Item;
                 that.log.info('- Снаряд удален:', '[' + bull.State + ']', bull);
             } catch (ex) {
                 that.log.exception(ex);
             }
         });
+    }
+
+    public onEveryTick(gameTickNum: number): void {
+        
+        // Отписаться от обработки событий через некоторое время
+        if (this.eventHits >= HITS_TO_STOP_EVENT_HANDLERS) {
+            if (this.globalStorage.currentAddedHandler) {
+                this.globalStorage.currentAddedHandler.disconnect();
+                delete this.globalStorage.currentAddedHandler;
+            }
+            if (this.globalStorage.currentRemovedHandler) {
+                this.globalStorage.currentRemovedHandler.disconnect();
+                delete this.globalStorage.currentRemovedHandler;
+            }
+        }
     }
 }
