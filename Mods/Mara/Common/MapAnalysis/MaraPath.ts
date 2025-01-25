cd45e1378cabc174a1828c6900c35fa4ab3e6df4
night-ghost-1 <nightgho5t@yandex.ru>
2025-01-25 16:57:55 +0300

    Fix path length calculation

--- Mods/Mara/Common/MapAnalysis/MaraPath.ts
+++ Mods/Mara/Common/MapAnalysis/MaraPath.ts
@@ -11,9 +11,13 @@ export class MaraPath {
         let length = 0;
 
         for (let i = 0; i < nodes.length - 1; i ++) {
-            length += MaraUtils.ChebyshevDistance(nodes[i], nodes[i + 1]);
+            length += MaraUtils.ChebyshevDistance(nodes[i].Region.Center, nodes[i + 1].Region.Center);
         }
 
         this.Length = length;
+    }
+
+    public ToString(): string {
+        return this.Nodes.map((n) => n.Region.Center.ToString()).join(", ");
     }
 }
