26a7c24d275b9a9d3f9a78308ab3c14a0f67b65a
Horde Resurrection <65279361+HordeResurrection@users.noreply.github.com>
2025-02-04 02:03:25 +0300

    fix Stride declarations

--- Scripts/declarations/stride-declarations.d.ts
+++ Scripts/declarations/stride-declarations.d.ts
@@ -1,6 +1,8 @@
 // В этом файле перечислены декларации-заглушки для типов из Stride
 
 declare namespace Stride.Core.Mathematics {
-    let Vector2: any; // https://doc.stride3d.net/latest/en/api/Stride.Core.Mathematics.Vector2.html
-    let Color: any;  // https://doc.stride3d.net/latest/en/api/Stride.Core.Mathematics.Color.html
+    /** https://doc.stride3d.net/latest/en/api/Stride.Core.Mathematics.Vector2.html */
+    class Vector2 { [key: string]: any; }
+    /** https://doc.stride3d.net/latest/en/api/Stride.Core.Mathematics.Color.html */
+    class Color { [key: string]: any; }
 }
