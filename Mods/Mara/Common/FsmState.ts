432936b801a16dad87902b324b9a67c084c87f18
dikys <lrdikysa@mail.ru>
2025-02-08 11:46:12 +0500

    Удалил лишние моды
    * Удалил лишние моды

--- Mods/Mara/Common/FsmState.ts
+++
@@ -1,6 +0,0 @@
-
-export abstract class FsmState {
-    abstract OnEntry(): void;
-    abstract OnExit(): void;
-    abstract Tick(tickNumber: number): void;
-}
--- Mods/Mara/Common/UnitComposition.ts
+++
@@ -1,2 +0,0 @@
-
-export type UnitComposition = Map<string, number>;
