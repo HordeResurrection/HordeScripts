a92328eaf48b0b04617cef46ee808178a8a4236f
dikys <lrdikysa@mail.ru>
2025-04-12 13:37:47 +0500

    обнова rev 0.75
    - реструктуризация директории
    - обнова для новой скрипт машиной

--- Mods/TowerProtection-Scripts/main.ts
+++
@@ -1,9 +0,0 @@
-import { activePlugins } from "active-plugins";
-import { TowerProtection } from "./TowerProtection";
-
-/**
- * Вызывается до вызова "onFirstRun()" при первом запуске скрипт-машины, а так же при hot-reload
- */
-export function onInitialization() {
-    activePlugins.register(new TowerProtection());
-}
